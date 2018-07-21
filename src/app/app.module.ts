3a6e60f16a6f214f7a1109b50563b99cc9c4d400
Jose Alcazar Lopez <jose.alcazar@external.wolterskluwer.com>
2018-07-21 14:00:57 +0200

    cambiada ruta principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-// tslint:disable-next-line:comment-format
 //Material
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
@@ -10,21 +9,21 @@ import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppComponent } from './app.component';
 import { GalleryComponent } from './gallery/gallery.component';
+import { KattegatComponent } from './kattegat/kattegat.component';
 import { Routes, RouterModule } from '@angular/router';
 
-import { KattegatComponent } from './kattegat/kattegat.component';
 
 const appRoutes: Routes = [
-  { path: 'gallery', component: GalleryComponent},
   { path: 'kattegat', component: KattegatComponent},
-  { path: '**', pathMatch: 'full', redirectTo: 'gallery' }
+  { path: 'gallery', component: GalleryComponent},
+  { path: '**', pathMatch: 'full', redirectTo: 'kattegat' }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    GalleryComponent,
-    KattegatComponent
+    KattegatComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule,
